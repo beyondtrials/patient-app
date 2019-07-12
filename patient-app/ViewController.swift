ed5a2832b78c5258eae35e719f8c2db690a82bcf
elliott <elliottbrunet@gmail.com>
2019-07-12 12:26:15 +0200

    Initial Commit

---
+++ patient-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  patient-app
+//
+//  Created by Elliott Brunet on 12.07.19.
+//  Copyright © 2019 Elliott Brunet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
